59dff727c33b3b0ca77a905ef434d683055e42b0
Alice Lindsay <beamgirl490@gmail.com>
2026-02-02 10:33:58 -0500

    .

---
+++ lib/profile_card.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class ProfileCard extends StatelessWidget {
+  const ProfileCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Responsive Design')),
+      body: Center(
+        child: Container(
+          color: Colors.grey[200],
+          padding: const EdgeInsets.all(20),
+          constraints: const BoxConstraints(maxWidth: 800),
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              if (constraints.maxWidth > 600) {
+                return Row(  //for a wide layout
+               // mainAxisSize: MainAxisSize.min,
+                  children: [
+                    _buildAvatar(),
+                    const SizedBox(width: 20),
+                    Expanded(child: _buildContent()),
+                  ],
+                );
+              }//end of if statment
+
+              else {
+                return Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  _buildAvatar(),
+                  SizedBox(height: 20),
+                  _buildContent(),
+                ],
+              );}
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+//function that returns a widget
+
+Widget _buildAvatar() {
+  return Center(
+    child: Container(
+      width: 100,
+      height: 100,
+      decoration: BoxDecoration(
+        color: Colors.lightGreenAccent,
+        shape: BoxShape.circle,
+      ),
+      child: Icon(Icons.person, size: 50, color: Colors.white),
+    ),
+  );
+}
+
+// content widget for the profile
+Widget _buildContent() {
+  return Center(
+    child: Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Some Name',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+        Text('Favorite Class cs:220'),
+        SizedBox(height: 20),
+        ElevatedButton(onPressed: () {}, child: Text('Log in')),
+      ],
+    ),
+  );
+} // end of content widget
